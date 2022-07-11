d906b9e78df380b5b1246117c0f51bc6d70dad6f
zhangbing520 <2281287648@qq.com>
2022-07-12 02:52:52 +0800

    commit

---
+++ react-hooks-redux-ts-arco-design/node_modules/arco-cli-dev-utils/lib/confirm.d.ts
@@ -0,0 +1,8 @@
+declare const _default: (checkFunc: any, message: any) => Promise<any>;
+/**
+ * Confirm
+ * @param checkFunc
+ * @param message
+ * @returns {Promise<boolean|*>}
+ */
+export default _default;
